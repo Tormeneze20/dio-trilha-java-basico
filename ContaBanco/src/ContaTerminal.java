b70eaaf8c78a792c44babc8426626d48db0387a5
thai <thaine.ormeneze@gmail.com>
2025-07-09 21:25:28 -0300

    Atualizando ContaTerminal.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       var scanner = new Scanner (System.in);
+       System.out.println("Informe o seu nome completo sem acentos ");
+       var name = scanner.nextLine();
+       System.out.println("Informe o número da sua agência");
+       var bankAgency = scanner.nextLine();
+       System.out.println("Informe o número da sua conta bancária");
+       var bankAccount = scanner.nextInt();
+       scanner.nextLine();
+       System.out.println("Informe o seu saldo da conta");
+       var bankBalance = scanner.nextFloat();
+       scanner.nextLine();
+       System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %s já está disponível para saque.", name, bankAgency, bankAccount, bankBalance);
+       scanner.close();
     }
 }
